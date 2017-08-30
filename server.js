9d20c16ff62c67891d1ae9b21646423d2e9337ce
Xu Liu <xu.liu@ygomi.com>
2017-08-30 11:34:20 +0800

    第一个nodejs的服务器完成

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
 var http = require("http");
+http.createServer(function(request,response){
+    console.log(request);
+	response.writeHead(200,{'Content-Type':'text/plain'});
+	response.end("Hello world\n");
+}).listen(12306);
+
+console.log("Start node server at prot:12306.");
