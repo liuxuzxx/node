8d169e23a76d070e4d5527bbabebac4bca822a05
Xu Liu <xu.liu@ygomi.com>
2017-10-16 18:29:15 +0800

    添加同步读取

--- file.js
+++ file.js
@@ -2,3 +2,11 @@ var fs = require("fs");
 
 var in_stream = fs.readFileSync('input.txt');
 console.log(in_stream.toString());
+
+var un_sync = fs.readFile("error.config",function(err,data){
+    if(err){
+        console.error(err);
+    }
+});
+
+console.log("读取不存在的文件对象错误展示...");
