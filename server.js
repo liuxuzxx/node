1ffa09ed8705163d6ff6897123ebecbc04b98b7b
Xu Liu <xu.liu@ygomi.com>
2017-10-17 14:19:43 +0800

    服务端读取html信息，并且在添加http的头部信息

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var http = require("http");
+var fs = require("fs");
 http.createServer(function(request,response){
     console.log(request);
-	response.writeHead(200,{'Content-Type':'text/plain'});
-	response.end("Hello world\n");
+	response.writeHead(200,{'Content-Type':'text/html',
+                            'Country-code':'China',
+                            'Camera-fov':'187'});
+	response.end(fs.readFileSync("./html/login.html").toString());
 }).listen(12306);
 
 console.log("Start node server at prot:12306.");
