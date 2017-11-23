ab09ff12497a62bcd5a07a3dba344080d9fbff74
Xu Liu <xu.liu@ygomi.com>
2017-11-23 18:24:33 +0800

    变换函数书写的地方

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 var http = require("http");
 var fs = require("fs");
-http.createServer(function(request,response){
+
+
+http.createServer(answer).listen(12306);
+
+function answer(request,response){
     console.log(request);
 	response.writeHead(200,{'Content-Type':'text/html',
                             'Country-code':'China',
                             'Camera-fov':'187'});
 	response.end(fs.readFileSync("./html/login.html").toString());
-}).listen(12306);
+}
 
 console.log("启动nodejs服务器，端口是：12306");
