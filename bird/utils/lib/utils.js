bd065d084fdebefde36e26399fd042cea1786043
lxzxx <928525166@qq.com>
2017-12-03 23:24:50 +0800

    添加对nodejs的模块使用

---
+++ bird/utils/lib/utils.js
@@ -0,0 +1,3 @@
+exports.format = function(data){
+	console.log("format:"+data.toString());
+};
---
+++ bird/utils/test/utils_test.js
@@ -0,0 +1,6 @@
+var utils_title = "NodeJs的工具类";
+console.log(utils_title);
+
+var utils = require("../lib/utils.js");
+
+utils.format(utils_title);
