ee32b29fe2b6bb9d44e0324eb645d90335fbbfe8
lxzxx <928525166@qq.com>
2018-03-21 22:43:17 +0800

    删除项目，使用express提供的框架

--- webftp/server.js
+++ webftp/server.js
@@ -1,17 +1,14 @@
-var http = require("http");
-var fs = require("fs");
+const express = require('express');
+const app = express();
+const hostName = 'localhost';
+const port = 2018;
 
-port = 10086;
 
-http.createServer(answer).listen(port);
+const server = app.listen(port,function(){
+	console.log(`WebFTP服务器启动，地址：http://${hostName}:${port}/`);
+});
 
-function answer(request,response){
-    console.log(request);
-	response.writeHead(301,{'Content-Type':'text/html',
-                            'Country-code':'China',
-                            'Camera-fov':'187',
-							'Location':"http://sina.com"});
-	response.end(fs.readFileSync("./html/login.html").toString());
-}
 
-console.log("启动nodejs服务器，端口是："+port);
+app.get('/',(req,res)=>{
+	res.send('你好，欢迎使用NodeJS版本的WebFTP。');
+});
--- webftp/test_file.js
+++
@@ -1,8 +0,0 @@
-var fs = require("fs");
-
-fs.readdir("/home/liuxu",function(err,files){
-	if(err){
-		console.log("读取文件列表信息错误...");
-	}
-	console.log(files);
-});
