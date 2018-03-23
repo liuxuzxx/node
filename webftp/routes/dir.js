a82498e360fde4f15f39548fad87bcac2136c754
Xu Liu <Xu.Liu@ygomi.com>
2018-03-23 17:55:58 +0800

    提交本地的代码

--- webftp/routes/dir.js
+++ webftp/routes/dir.js
@@ -1,12 +1,9 @@
 const express = require('express');
+const fs = require('fs');
 const router = express.Router();
 
-router.get('/',(req,res)=>{
-	res.send('服务器可以进入目录了...');
-});
-
-router.get('/home/*',(req,res)=>{
-	res.send('访问地址是:'+req.params[0]);
+router.get('/*',(req,res)=>{
+    res.send(fs.readdir('/home/user'));
 });
 
 module.exports=router;
