369b3f8d1910d01f808acb2691a221af8b048d47
liuxu <18270719298@163.com>
2018-05-28 14:02:35 +0800

    添加http-proxy模块的实验，其实就是一个拦截器，拿到这个host信息，然后根据这个host进行switch的转发给其他的模块，类似于nginx

--- scout/first.js
+++ scout/first.js
@@ -1,14 +1,36 @@
 const http = require('http');
+const httpProxy = require("http-proxy");
 
 const hostName = 'localhost';
 const port = 2018;
 
-const server = http.createServer((req,res)=>{
-    res.statusCode = 200;
-    res.setHeader('Content-Type','application/json');
-    res.end('侦查兵...');
+const proxy = httpProxy.createProxyServer({});
+proxy.on('error', function (err, req, res) {
+    res.writeHead(500, {
+        'Content-Type': 'text/plain'
+    });
+    res.end('服务器出现错误，请等待管理员处理');
 });
 
-server.listen(port,hostName,()=>{
+const server = http.createServer(function(req, res){
+    var host = req.headers.host;
+    console.log(`请求的域名是：${host}`);
+    switch (host) {
+        case 'localhost:2018':
+            res.statusCode = 200;
+            res.setHeader('Content-Type','application/json');
+            res.end('{"Host地址信息":"Localhost地址信息"}');
+            break;
+        default:
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.end('{"info":"侦查兵...update"}');
+    }
+});
+
+server.listen(port, hostName, function(){
     console.log(`服务器启动了，监听地址是:http://${hostName}:${port}/`);
 });
+
+
+
