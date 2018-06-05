c282b1a2ce00205c0a45484ab023dca3feb23922
liuxu <18270719298@163.com>
2018-06-05 13:51:52 +0800

    添加AngularJS的学习页面

--- scout/server.js
+++ scout/server.js
@@ -1,9 +1,13 @@
 const http = require('http');
 const httpProxy = require("http-proxy");
-const curl = require('./service/curl-man/curl-man');
+const fs = require("fs");
+const url = require("url");
 
 const hostName = 'localhost';
 const port = 2018;
+
+const curl = require('./service/curl-man/curl-man');
+const angular = require('./service/angular/angular');
 
 /**
  * 生成一个代理服务器，然后使用这个proxy进行前端请求的转发工作
@@ -16,28 +20,13 @@ proxy.on('error', function (err, req, res) {
     res.end('服务器出现错误，请等待管理员处理');
 });
 
-const server = http.createServer(function(req, res){
-    var host = req.headers.host;
-    console.log(`请求的域名是：${host}`);
-    switch (host) {
-        case 'localhost:2018':
-            res.statusCode = 200;
-            res.setHeader('Content-Type','application/json');
-            res.end('{"Host地址信息":"Localhost地址信息"}');
-            break;
-        case '127.0.0.1:2018':
-            res.statusCode = 200;
-            res.setHeader('Content-Type','application/json');
-            res.end('{"127的地址信息":"127.0.0.1:2018"}');
-            break;
-        default:
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'application/json');
-            res.end('{"info":"侦查兵...update"}');
-    }
-});
-
-server.listen(port, hostName, function(){
+http.createServer(function(req, res){
+    let requestMessage = req.headers;
+    console.log(JSON.stringify(requestMessage));
+    let path = url.parse(req.url).pathname;
+    console.log(`客户端请求的文件路径为：${path}`);
+    angular.urlDispatcher(path,res);
+}).listen(port, hostName, function(){
     console.log(`服务器启动了，监听地址是:http://${hostName}:${port}/`);
     var curlMessage = curl.license();
     console.log(curlMessage)
---
+++ scout/service/angular/angular.js
@@ -0,0 +1,32 @@
+'use strict'
+const fs = require('fs');
+/**
+ * 对传递过来的url进行转发，根据不同的规则
+ * @param path url路径
+ * @param response 回复给客户端的Response对象
+ */
+String.prototype.endWith=function(endStr){
+    var d=this.length-endStr.length;
+    return (d>=0&&this.lastIndexOf(endStr)==d)
+}
+
+function urlDispatcher(path,response){
+    if(path.endWith('.html')){
+        fs.readFile("."+path,function(error,data){
+            if(error){
+                console.log(error);
+                response.writeHead(404, {'Content-Type': 'text/html'});
+            }else{
+                response.writeHead(200, {'Content-Type': 'text/html'});
+                response.write(data.toString());
+            }
+            response.end();
+        });
+    }else{
+        response.writeHead(206,{'Content-Type':'application/json','User-Site':'liuxu'});
+        response.write('{"site":"localhost-angular-site"}');
+        response.end();
+    }
+}
+
+exports.urlDispatcher=urlDispatcher;
