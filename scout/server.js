eee71f5b08809aa97d38384168ba94a40bdd9614
liuxu <18270719298@163.com>
2018-12-05 17:12:35 +0800

    Push update

--- scout/server.js
+++ scout/server.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
-app.use(express.static(__dirname + '/public'));
-app.listen(6000);
+app.use('/public',express.static('src/public'));
+app.listen(12306);
+
 app.get('/',function (request,response) {
     response.write('hello world');
+    response.end();
+});
+
+app.get('/index',function(request,response){
+    response.write('首页界面信息,查看Express的路由拦截');
+    response.end();
 });
 
 
